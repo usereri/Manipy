5582fc30592280c7663b4c7b90cba9ace00ccad4
eri <eryk.jamroz@gmail.com>
2025-03-04 21:50:54 +0100

    Added nodes for rviz2 and joint_state_publisher

--- src/robot_description/launch/display.launch.py
+++ src/robot_description/launch/display.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
 import os
 from ament_index_python import get_package_share_directory
 
@@ -12,11 +13,24 @@ def generate_launch_description():
         description="Absolute path to the robot URDF file"
         )
     
-    robot_description = ParameterValue()
+    robot_description = ParameterValue(Command(["xacro", LaunchConfiguration("model")]))
 
     robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": robot_description}]
+    )
+
+    joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen"
+
     )
     return LaunchDescription([])
